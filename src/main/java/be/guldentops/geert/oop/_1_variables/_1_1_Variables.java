9f54f16a60f5df112dc76e54baf078f486944a0d
Geert Guldentops <geert.guldentops@corsearch.com>
2019-05-16 20:00:18 +0200

    Exercise 1.1

---
+++ src/main/java/be/guldentops/geert/oop/_1_variables/_1_1_Variables.java
@@ -0,0 +1,11 @@
+package be.guldentops.geert.oop._1_variables;
+
+public class _1_1_Variables {
+
+	public static void main(String[] args) {
+		String firstName = "Geert";
+		String lastName = "Guldentops";
+
+		System.out.println(firstName + " " + lastName);
+	}
+}
