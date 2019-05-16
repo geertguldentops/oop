0a1a5d4f86419bf9bc7f68001244e002fe777034
Geert Guldentops <geert.guldentops@corsearch.com>
2019-05-16 20:05:20 +0200

    Exercise 1.4

---
+++ src/main/java/be/guldentops/geert/oop/_1_variables/_1_4_Variables.java
@@ -0,0 +1,14 @@
+package be.guldentops.geert.oop._1_variables;
+
+public class _1_4_Variables {
+
+	public static void main(String[] args) {
+		double a = 42.0;
+		double b = 33.0;
+
+		System.out.println(a + b);
+		System.out.println(a - b);
+		System.out.println(a * b);
+		System.out.println(a / b); // Does NOT lose precision anymore!
+	}
+}
