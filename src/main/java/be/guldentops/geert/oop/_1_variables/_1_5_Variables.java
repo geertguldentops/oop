7884990f0e47601ff689fc9e472adcbcbc332e73
Geert Guldentops <geert.guldentops@corsearch.com>
2019-05-16 20:08:53 +0200

    Exercise 1.5

---
+++ src/main/java/be/guldentops/geert/oop/_1_variables/_1_5_Variables.java
@@ -0,0 +1,11 @@
+package be.guldentops.geert.oop._1_variables;
+
+public class _1_5_Variables {
+
+	public static void main(String[] args) {
+		int a = 5;
+		double b = 8.7;
+
+		System.out.printf("The multiplication of %d and %.2f is %.2f", a, b, a * b);
+	}
+}
