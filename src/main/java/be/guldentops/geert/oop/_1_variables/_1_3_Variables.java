a4e4c7ef8e9568866b71670eedd23ef775015b77
Geert Guldentops <geert.guldentops@corsearch.com>
2019-05-16 20:04:00 +0200

    Exercise 1.3

---
+++ src/main/java/be/guldentops/geert/oop/_1_variables/_1_3_Variables.java
@@ -0,0 +1,11 @@
+package be.guldentops.geert.oop._1_variables;
+
+public class _1_3_Variables {
+
+	public static void main(String[] args) {
+		int a = 42;
+		int b = 33;
+
+		System.out.println(a % b);
+	}
+}
